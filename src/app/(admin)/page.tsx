a4eb6e97acfd9867f73f34d5eb654e3f6bc2b1aa
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-03-09 12:56:27 +1300

    add sidebar

---
+++ src/app/(admin)/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+
+import { useSidebar } from "@/context/SidebarContext";
+import AppSidebar from "@/";
---
+++ src/layout/AppSidebar.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useSidebar } from "@/context/SidebarContext";
+import { LayoutGrid, Users } from "lucide-react";
+
+type NavItem = {
+  name: string;
+  icon: React.ReactNode;
+  path: string;
+};
+
+const navItems: NavItem[] = [
+  {
+    name: "Home",
+    icon: <LayoutGrid size={18} />,
+    path: "/home",
+  },
+  {
+    name: "Employee",
+    icon: <Users size={18} />,
+    path: "/employee",
+  },
+];
+
+const AppSidebar: React.FC = () => {
+  const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
+  const pathname = usePathname();
+
+  const renderMenuItems = (navItems: NavItem[]) => (
+    <ul className="flex flex-col gap-2">
+      {navItems.map((nav, index) => (
+        <li key={nav.name}>
+          {nav.path && (
+            <Link
+              href={nav.path}
+              className={`menu-item group ${
+                isActive(nav.path) ? "menu-item-active" : "menu-item-inactive"
+              }`}
+            >
+              <span>{nav.icon}</span>
+              {(isExpanded || isHovered || isMobileOpen) && (
+                <span className="menu-item-text">{nav.name}</span>
+              )}
+            </Link>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+
+  const isActive = useCallback((path: string) => path === pathname, [pathname]);
+
+  return (
+    <aside
+      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 left-0 px-5 bg-white text-gray-900 h-screen z-50 border-r border-gray-200
+        ${
+          isExpanded || isMobileOpen
+            ? "w-[290px]"
+            : isHovered
+            ? "w-[290px]"
+            : "w-[90px]"
+        }
+        ${isMobileOpen ? "translate-x-0" : "-translate-x-full"}
+        lg:translate-x-0
+        `}
+      onMouseEnter={() => !isExpanded && setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div
+        className={`py-8 flex ${
+          !isExpanded && !isHovered ? "lg:justify-center" : "justify-start"
+        }`}
+      >
+        <Link href="/">
+          {isExpanded || isHovered || isMobileOpen ? (
+            <Image
+              src="/images/logo/logo.svg"
+              alt="Logo"
+              width={150}
+              height={40}
+            />
+          ) : (
+            <Image
+              src="/images/logo/logo-icon.svg"
+              alt="Logo"
+              width={32}
+              height={32}
+            />
+          )}
+        </Link>
+      </div>
+      <div className="flex flex-col overflow-y-auto">
+        <nav className="mb-6">{renderMenuItems(navItems)}</nav>
+      </div>
+    </aside>
+  );
+};
+
+export default AppSidebar;
